23438a9d0b5b68cb058dd4b06869f07f0e4987b4
Chelladurai.s-at-994548211847 <chelladurai.s@yourlmo.com>
2024-01-01 08:35:33 +0530

    Docker file added

--- app.js
+++ app.js
@@ -56,7 +56,18 @@ app.get("/getComments/:url", (req, res) => {
   // Helper function to validate and clean text
 
   async function scrapeComments(videoUrl, maxScrolls) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
     const page = await browser.newPage();
     await page.goto(videoUrl);
 
